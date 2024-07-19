64d9e3d498252d5942eae20a29d3ebee7bf2f691
Andy Pan <andypan@umich.edu>
2024-07-19 15:56:39 -0400

    fix results

--- predictor.py
+++ predictor.py
@@ -11,43 +11,58 @@ matches["Hour"] = matches["Time"].str.replace(":.+", "", regex = True).astype("i
 matches["Day_Code"] = matches["Date"].dt.dayofweek
 matches["Target"] = matches["Result"].astype("category").cat.codes #D:0, L:1, W:2
 
+future_matches = pd.read_csv("future_matches.csv", index_col=0)
+
+future_matches["Date"] = pd.to_datetime(future_matches["Date"])
+future_matches["Venue_Code"] = future_matches["Venue"].astype("category").cat.codes
+future_matches["Opp_Code"] = future_matches["Opponent"].astype("category").cat.codes
+future_matches["Day_Code"] = future_matches["Date"].dt.dayofweek
+future_matches["Target"] = -99
+
 rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
-train = matches[matches["Date"] < '2024-01-01'] 
-test = matches[matches["Date"] > '2024-01-01'] 
-predictors = ["Venue_Code", "Opp_Code", "Hour", "Day_Code"]
+train = matches
+test = future_matches
+predictors = ["Venue_Code", "Opp_Code", "Day_Code"]
 rf.fit(train[predictors], train["Target"])
 preds = rf.predict(test[predictors])
 
 acc = accuracy_score(test["Target"], preds)
 
 combined = pd.DataFrame(dict(actual = test["Target"], prediction = preds))
-print(pd.crosstab(index = combined["actual"], columns=combined["prediction"]))
-print(precision_score(test["Target"], preds, average='weighted'))
+# print(pd.crosstab(index = combined["actual"], columns=combined["prediction"])) for testing
+# print(precision_score(test["Target"], preds, average='weighted')) for testing
 
 def rolling_avg(group, cols, new_cols):
     group = group.sort_values("Date")
-    rolling_stats = group[cols].rolling(3, closed="left").mean()
+    rolling_stats = group[cols].rolling(3, min_periods=1, closed="left").mean()
     group[new_cols] = rolling_stats
-    group = group.dropna(subset = new_cols)
     return group
 
 cols = ["GF", "GA", "Sh", "SoT", "Dist", "FK", "PK", "PKatt"]
 new_cols = [f"{c}_rolling" for c in cols]
-matches_rolling = matches.groupby("Team").apply(lambda x: rolling_avg(x, cols, new_cols))
-matches_rolling = matches_rolling.droplevel("Team")
-matches_rolling.index = range(matches_rolling.shape[0])
+
+for col in cols:
+    if col not in future_matches.columns:
+        future_matches[col] = 0
+
+combined_matches = pd.concat([matches, future_matches], ignore_index=True)
+combined_matches_rolling = combined_matches.groupby("Team").apply(lambda x: rolling_avg(x, cols, new_cols))
+combined_matches_rolling = combined_matches_rolling.droplevel("Team")
+combined_matches_rolling.index = range(combined_matches_rolling.shape[0])
+combined_matches_rolling.to_csv("h.csv") #for debugging
+
 
 def make_predict(matches, predictors):
-    train = matches[matches["Date"] < '2024-01-01'] 
-    test = matches[matches["Date"] > '2024-01-01'] 
+    train = matches[matches["Date"] < '2024-08-01']
+    test = matches[matches["Date"] >= '2024-08-01']
     rf.fit(train[predictors], train["Target"])
     preds = rf.predict(test[predictors])
     combined = pd.DataFrame(dict(actual = test["Target"], prediction = preds))
     precision = precision_score(test["Target"], preds, average='weighted')
     return combined, precision 
 
-combined, precision = make_predict(matches_rolling, predictors + new_cols)
-combined = combined.merge(matches_rolling[["Date", "Team", "Opponent", "Result"]], left_index=True, right_index=True)
+combined, precision = make_predict(combined_matches_rolling, predictors + new_cols)
+combined = combined.merge(combined_matches_rolling[["Date", "Team", "Opponent", "Result"]], left_index=True, right_index=True)
 
 # class MissDict(dict):
 #     __missing__ = lambda self, key: key
@@ -59,5 +74,9 @@ combined = combined.merge(matches_rolling[["Date", "Team", "Opponent", "Result"]
 # combined["new_team"] = combined["Team"].map(mapping)
 
 merged = combined.merge(combined, left_on = ["Date", "Team"], right_on = ["Date", "Opponent"])
+merged.to_csv("lol.csv")
+print(merged)
 print(merged[(merged["prediction_x"] == 2) & (merged["prediction_y"] == 1)]["actual_x"].value_counts())
+print(merged[(merged["prediction_x"] == 1) & (merged["prediction_y"] == 2)]["actual_x"].value_counts())
+print(merged[(merged["prediction_x"] == 0) & (merged["prediction_y"] == 0)]["actual_x"].value_counts())
 
--- scraping.py
+++ scraping.py
@@ -4,7 +4,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 standings_url = "https://fbref.com/en/comps/12/La-Liga-Stats"
-years = list(range(2024, 2021, -1))
+years = list(range(2025, 2020, -1))
 all_matches = []
 for year in years:
     data = requests.get(standings_url)
@@ -44,3 +44,25 @@ for year in years:
 
 match_df = pd.concat(all_matches)
 match_df.to_csv("matches.csv")
+
+next_season_url = "https://fbref.com/en/comps/12/2024-2025/2024-2025-La-Liga-Stats"
+data = requests.get(next_season_url)
+soup = BeautifulSoup(data.text)
+standings = soup.select('table.stats_table')[0] 
+links = [l.get("href") for l in standings.find_all('a')] 
+links = [l for l in links if '/squads' in l] 
+team_urls = [f"https://fbref.com{l}" for l in links] 
+all_matches = []
+for url in team_urls:
+    name = url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+    data = requests.get(url)
+    matches = pd.read_html(data.text, match = "Scores & Fixtures")[0]
+    matches = matches[matches["Comp"] == "La Liga"]
+    matches["Season"] = year
+    matches["Team"] = name
+    all_matches.append(matches)
+    time.sleep(4)
+
+future_match_df = pd.concat(all_matches)
+future_match_df.to_csv("future_matches.csv")
+
