95c63cda0e4fa76bbb9cc3b323f07a1033e34a21
Andy Pan <andypan@umich.edu>
2024-07-20 20:09:26 -0400

    work in progress

--- filter.py
+++ filter.py
@@ -1,18 +1,17 @@
 import pandas as pd
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 results = pd.read_csv('data.csv')
-
 
 date = []
 opponent = []
 prediction = []
 
-@app.route("/")
+@app.route("/checkTeam", methods = ['POST'])
 def showTeam():
     if request.method == 'POST':
-        chosen_team = request.form['team']
+        chosen_team = request.form['name']
     for _, x in results[results["Team"] == chosen_team].iterrows():
         date.append(x["Date"])
         opponent.append(x["Opponent"])
@@ -22,6 +21,7 @@ def showTeam():
             prediction.append('L')
         else:
             prediction.append('D')
+    return jsonify(date = date, opponent = opponent, prediction = prediction)
 
 if __name__ == "__main__": 
     app.run(debug=True)
